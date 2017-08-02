4a9fcc0a5526188781a403be3eb72359358dcfa5
USER <user@USER-no-MacBook-Air.local>
2017-08-02 13:49:29 +0900

    Initial Commit

---
+++ friesornot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  friesornot
+//
+//  Created by USER on 8/2/17.
+//  Copyright © 2017 Riiga Toyosaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
